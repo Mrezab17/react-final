5a515c6048e93daff750c50725e1aa6e9c7595a2
mreza <mrezabmiri17@gmail.com>
2023-08-30 14:19:29 +0330

    Store Provided to The App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import ShopRouter from "./components/ShopRouter";
 
+import { Provider } from "react-redux";
+import store from "./store/store";
+import { persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 const App = () => {
   return (
     <>
-      <ShopRouter />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <ShopRouter />
+        </PersistGate>
+      </Provider>
     </>
   );
 };
