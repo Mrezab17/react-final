7d28d18e8d78254ec98ec9caa1ba7b5e9d67dffe
mreza <mrezabmiri17@gmail.com>
2023-08-30 14:18:51 +0330

    Store Created And Products Slice Added .

---
+++ src/store/store.js
@@ -0,0 +1,24 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+import productsReducer from "./productsSlice";
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
+export default store;
+export const persistor = persistStore(store);
