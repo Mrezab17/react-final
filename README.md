2a0a7479b7ad4755f90898bc9d079a50a355e097
mreza <mrezabmiri17@gmail.com>
2023-08-30 10:46:23 +0330

    Init with Vite . dir set to rtl . Tailwind Installed .

---
+++ README.md
@@ -0,0 +1,1 @@
+React Final Quiz For The Company of Samin Rai
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <>
+      <div className="bg-green-600 w-screen h-screen"></div>
+    </>
+  );
+};
+
+export default App;
