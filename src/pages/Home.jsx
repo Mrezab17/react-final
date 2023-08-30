3bd484d19ef2ca1a26950289e61cc87165e97725
mreza <mrezabmiri17@gmail.com>
2023-08-30 14:20:09 +0330

    products imported at Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProductsData } from "../store/productsActions";
+import { useEffect } from "react";
+
 const Home = () => {
+  const isFetched = useSelector((state) => state.products.isFetched);
+  const products = useSelector((state) => state.products.items);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!isFetched) dispatch(fetchProductsData());
+  }, [dispatch, isFetched]);
+
   return (
     <>
       <div className="h-full bg-primary"></div>
